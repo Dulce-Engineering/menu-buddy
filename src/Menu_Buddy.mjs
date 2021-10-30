126a60c8a5aeca308ee0a635fc5026903ea942d7
Esteban Ramirez <netssrmrz@yahoo.com.au>
2021-10-30 14:30:36 +1100

    update

---
+++ src/Menu_Buddy.mjs
@@ -0,0 +1,219 @@
+
+  class Menu_Buddy extends HTMLElement
+  {
+    static name = "menu-buddy";
+
+    constructor() 
+    {
+      super();
+      this.attachShadow({mode: 'open'});
+      this.On_Open_Btn_Click = this.On_Open_Btn_Click.bind(this);
+      this.Toggle = this.Toggle.bind(this);
+      this.Hide = this.Hide.bind(this);
+      this.On_Select_Btn_Click = this.On_Select_Btn_Click.bind(this);
+    }
+
+    connectedCallback()
+    {
+      this.Render();
+    }
+
+    static observedAttributes = ["style-src", "show"];
+    attributeChangedCallback(attrName, oldValue, newValue)
+    {
+      if (attrName == "style-src")
+      {
+        this.style_src = newValue;
+      }
+      else if (attrName == "show")
+      {
+        this.show = newValue;
+      }
+    }
+
+    set menu(value)
+    {
+      this.menu_def = value;
+      this.Render();
+    }
+
+    // Events =====================================================================================
+
+    On_Select_Btn_Click(event, option)
+    {
+      const new_event = new CustomEvent("clickoption", {detail: option});
+      this.dispatchEvent(new_event);
+    }
+
+    On_Open_Btn_Click(event, menu_div, option_div)
+    {
+      this.Open(option_div, menu_div);
+    }
+
+    On_This_Click(event)
+    {
+      event.stopPropagation();
+    }
+
+    // Rendering ==================================================================================
+
+    Open(open_elem, close_elem)
+    {
+      if (close_elem)
+      {
+        close_elem.style.width = "0px";
+      }
+      if (open_elem)
+      {
+        open_elem.style.width = this.offsetWidth + "px";
+      }
+    }
+
+    Close_All()
+    {
+      for (const menu_div of this.shadowRoot.children)
+      {
+        menu_div.style.width = "0px";
+      }
+    }
+
+    Render()
+    {
+      this.addEventListener("click", this.On_This_Click);
+
+      if (this.style_src)
+      {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = this.style_src;
+        this.shadowRoot.replaceChildren(link);
+      }
+
+      this.root_div = this.Create_Menu(this.shadowRoot, this.menu_def);
+      if (this.show)
+      {
+        this.Show();
+      }
+    }
+
+    Create_Menu(parent, menu, is_closed, parent_div)
+    {
+      let menu_div;
+
+      if (menu)
+      {
+        menu_div = document.createElement("div");
+        menu_div.style.width = "0px";
+        menu_div.id = menu.id;
+        menu_div.classList.add(menu.class_name);
+        menu.elem = menu_div;
+        parent.append(menu_div);
+
+        const close_btn = document.createElement("button");
+        close_btn.style.whiteSpace = "nowrap";
+        if (parent_div)
+        {
+          close_btn.innerHTML = "<span class='menu_title_prev'>&bigtriangleup;</span><span>" + menu.title + "</span>";
+          close_btn.addEventListener("click", (event) => this.On_Open_Btn_Click(event, menu_div, parent_div));
+        }
+        else
+        {
+          close_btn.innerText = menu.title;
+        }
+        close_btn.classList.add("menu_title");
+        menu_div.append(close_btn);
+
+        for (const option of menu.options)
+        {
+          let option_btn;
+
+          if (option.options)
+          {
+            option_btn = document.createElement("button");
+            option_btn.style.whiteSpace = "nowrap";
+            option_btn.innerHTML = "<span>" + option.title + "</span><span class='menu_option_next'>&bigtriangleup;</span>";
+            const option_div = this.Create_Menu(parent, option, true, menu_div);
+            option_btn.addEventListener("click", (event) => this.On_Open_Btn_Click(event, menu_div, option_div));
+          }
+          else if (typeof(option.title) == "object")
+          {
+            option_btn = option.title;
+          }
+          else
+          {
+            option_btn = document.createElement("button");
+            option_btn.style.whiteSpace = "nowrap";
+            option_btn.innerText = option.title;
+            option_btn.addEventListener("click", event => this.On_Select_Btn_Click(event, option));
+          }
+          option_btn.classList.add("menu_option");
+          menu_div.append(option_btn);
+        }
+      }
+
+      return menu_div;
+    }
+
+    Toggle()
+    {
+      let res;
+
+      if (this.style.display == "inline-block")
+      {
+        this.Hide();
+        res = false;
+      }
+      else
+      {
+        this.Show();
+        res = true;
+      }
+
+      return res;
+    }
+
+    Hide()
+    {
+      this.style.display = "none";
+    }
+
+    Show()
+    {
+      this.style.left = "";
+      this.style.top = "";
+      this.Close_All();
+      this.style.display = "inline-block";
+      this.Open(this.root_div, null);
+      this.Confine();
+    }
+
+    Confine()
+    {
+      const padding = 10;
+      const docRect = window.document.body.getBoundingClientRect();
+      const thisRect = this.getBoundingClientRect();
+      let x = thisRect.x;
+      let y = thisRect.y;
+
+      if (thisRect.right + padding > docRect.right)
+      {
+        x -= thisRect.right - docRect.right + padding;
+      }
+      if (thisRect.left - padding < docRect.left)
+      {
+        x += docRect.left - thisRect.left + padding;
+      }
+      if (thisRect.bottom + padding > docRect.bottom)
+      {
+        y -= thisRect.bottom - docRect.bottom + padding;
+      }
+      if (thisRect.top + padding < docRect.top)
+      {
+        y += docRect.top - thisRect.top + padding;
+      }
+      this.style.left = x + "px";
+      this.style.top = y + "px";
+    }
+  }
+
+  export default Menu_Buddy;
